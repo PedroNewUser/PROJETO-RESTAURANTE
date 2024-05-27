0196a4f13bbd709bb49ac0c0c46b30080437e698
PedroNewUser <141732147+PedroNewUser@users.noreply.github.com>
2024-05-27 15:52:46 -0300

    Delete Restaurante_TemperoRegional directory

--- Restaurante_TemperoRegional/app.js
+++
@@ -1,8 +0,0 @@
-const searchicon1 = document.querySelector('#searchicon1')
-const srchicon1 = document.querySelector('#srchicon1');
-const search1 = document.querySelector( '#searchinput1');
-
-searchicon1.addEventListener('click', function(){
-    search1.style.display = 'flex';
-    searchicon1.style.display = 'none';
-})
